805434c1b06c2de499b453c9f93bd294d16e4b00
Vitor Ferronato <vitorferronato@gmail.com>
2022-03-12 15:41:51 -0300

    Update

--- js/script.js
+++ js/script.js
@@ -2,18 +2,14 @@ const computerChoiceDisplay = document.getElementById('computer-choice')
 const userChoiceDisplay = document.getElementById('user-choice')
 const resultDisplay = document.getElementById('result')
 const possibleChoices = document.querySelectorAll('button') 
-const score = document.getElementById('score')
+const scorePlayer = document.getElementById('score-player')
+const scoreComputer = document.getElementById('score-computer')
+
 let userChoice
 let computerChoice
 let result
+let computerScore = 0
 let userScore = 0
-
-possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
-    userChoice= e.target.id
-    userChoiceDisplay.innerHTML = userChoice 
-    generateComputerChoice()
-    getResult()
-}))
 
 function generateComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3) +1
@@ -30,37 +26,51 @@ function generateComputerChoice() {
     computerChoiceDisplay.innerHTML = computerChoice
 }
 
-function getResult() {
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    userChoice= e.target.id
+    userChoiceDisplay.innerHTML = userChoice 
+    generateComputerChoice()
+    getResult()
+}))
 
+function scoreAddPlayer() {
+    userScore += 1
+    scorePlayer.innerHTML = userScore
+}
+
+function scoreAddComputer() {
+    computerScore += 1
+    scoreComputer.innerHTML = computerScore
+}
+
+function getResult() {
     if (computerChoice === userChoice) {
         result = 'its a Draw'
     }
     if (computerChoice === 'rock' && userChoice ==='paper') {
         result = 'You win!'
-        scoreAdd()
+        scoreAddPlayer()
     }
     if (computerChoice === 'rock' && userChoice ==='scissor') {
         result = 'You lost!'
+        scoreAddComputer()
     }
     if (computerChoice === 'paper' && userChoice ==='scissor') {
         result = 'You win!'
-        scoreAdd()
+        scoreAddPlayer()
     }
     if (computerChoice === 'paper' && userChoice ==='rock') {
         result = 'You lost!'
+        scoreAddComputer()
     }
     if (computerChoice === 'scissor' && userChoice ==='rock') {
         result = 'You win!'
-        scoreAdd()
+        scoreAddPlayer()
     }
     if (computerChoice === 'scissor' && userChoice ==='paper') {
         result = 'You lost!'
+        scoreAddComputer()
     }
     resultDisplay.innerHTML = result
 }
 
-function scoreAdd() {
-    userScore += 1
-    score.innerHTML = userScore
-
-}
